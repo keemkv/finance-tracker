227b3051f259827eee7298a2b3cd9f6f32da1216
keemkv <keemkv@gmail.com>
2026-07-31 16:08:03 +0900

    Use Railway API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://finance-tracker-7gfm0vhj7-konstantin7.vercel.app/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
